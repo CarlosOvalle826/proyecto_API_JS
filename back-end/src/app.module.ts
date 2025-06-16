6f691f04a3e3d8be9c33913e44270374ffbd6ba6
Carlos Ovalle <covalle826@gmail.com>
2025-06-15 23:07:21 -0600

    feat(backend): Configuración conexión a MongoDB

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose'; // Importa MongooseModule
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://covalle826:tHAw7225bfTycdXh@cluster0.ltoaoe2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0]'),
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
